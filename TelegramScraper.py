e35ad7bb3d6f5a5d4966ffa88d3db4c11bfe97ca
Tomer258 <tomer25898@gmail.com>
2025-04-17 08:12:03 +0300

    test

--- TelegramScraper.py
+++ TelegramScraper.py
@@ -9,4 +9,5 @@ def load_scraper():
     load_dotenv()
     api_id = int(os.getenv("API_ID"))
     api_hash = os.getenv("API_HASH")
+    print('yo')
     print(f"API ID: {api_id}, API HASH: {api_hash[:5]}***")

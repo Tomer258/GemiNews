e48c7ced0cd09fae76f9f253bce643334cec40fa
Tomer258 <tomer25898@gmail.com>
2025-04-17 08:05:15 +0300

    base works

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    print(TelegramScraper.hello())
+    TelegramScraper.load_scraper()
     return 'Hello, Flask!'
 
 if __name__ == '__main__':
